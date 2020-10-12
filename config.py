dcbbf4da8aece7d95723cad26b5f55584cbad9bd
Zhornovyi <47090110+Zhornovyi@users.noreply.github.com>
2020-10-12 19:40:15 +0300

    Added response verification, using imported libraries' Exceptions instead of base. Code is cleaned up.

--- config.py
+++ config.py
@@ -1,55 +1,18 @@
 import logging
 
 logging_config = dict(
-    version = 1,
-    formatters = {
+    version=1,
+    formatters={
         'f': {'format':
-              '[%(levelname)s] - Line: %(lineno)d - %(name)s - : %(message)s.'}
-        },
-    handlers = {
+                  '[%(levelname)s] - Line: %(lineno)d - %(name)s - : %(message)s.'}
+    },
+    handlers={
         'h': {'class': 'logging.StreamHandler',
               'formatter': 'f',
               'level': logging.ERROR}
-        },
-    root = {
+    },
+    root={
         'handlers': ['h'],
         'level': logging.ERROR,
-        },
+    },
 )
-
-# config = {
-#     "apiBaseUrl": ZOHO_API_URL,
-#     "token_persistence_path": "/Users/yehorzhornovyi/PycharmProjects/ga_zoho_integration/env/",
-#     "currentUserEmail": ZOHO_LOGIN_EMAIL,
-#     "client_id": ZOHO_CLIENT_ID,
-#     "client_secret": ZOHO_CLIENT_SECRET,
-#     "redirect_uri": "https://coxit.co",
-#     "accounts_url": "https://accounts.zoho."+ZOHO_ACCOUNT_DOMEN,
-# }
-
-# ZOHO_LOGIN_EMAIL = "yehorzhornovyi@gmail.com"
-# ZOHO_GRANT_TOKEN = "1000.58ee4e53d9f5e522e2d842d66d4e08a9.62282898ecfb46fdc051f20db36bc8fe"
-# ZOHO_CLIENT_ID = "1000.6UBJGFQ9ZBEU8R4OF9X35FZ4KROLJO"
-# ZOHO_CLIENT_SECRET = "538947a51fc6138b864550f447e75d8c3ba2edd0c3"
-# ZOHO_ACCOUNT_DOMEN = "eu"
-#
-# ZOHO_API_URL = "https://www.zohoapis." + ZOHO_ACCOUNT_DOMEN
-# DEALS_ENDPOINT = "/crm/v2/Deals"
-# ENABLE_NOTIFICATIONS_ENDPOINT = "/crm/v2/actions/watch"
-# ZOHO_NOTIFICATIONS_ENDPOINT = "/zoho/deals/change"
-# GOOGLE_ANALYTICS_API_URL = "https://www.google-analytics.com"
-# GOOGLE_ANALYTICS_COLLECT_ENDPOINT = "/collect"
-
-# params = {
-#                 "v": "1",
-#                 "t": "event",
-#                 "tid": "UA-180087680-1",
-#                 "uid": user.generate_unique_id(),
-#                 "ec": "zoho_stage_change",
-#                 "ea": "stage_change",
-#                 "el": current_stage,
-#                 "ua": "Opera / 9.80"
-#             }
-
-# host = "127.0.0.1"
-# port = 5050
--- main.py
+++ main.py
@@ -1,19 +1,22 @@
 import json
 import logging
+import sys
 from logging.config import dictConfig
 from os import path
-from flask import Flask, request, Response
+
 import requests
-import config
 import zcrmsdk as zoho_crm
+from flask import Flask, request, Response
 from pyga.entities import Visitor
 
+import config
+
 dictConfig(config.logging_config)
-logger = logging.getLogger()
+LOGGER = logging.getLogger()
 
 # TODO: make following data configurable on script startup (passed as arguments)
 ZOHO_LOGIN_EMAIL = "yehorzhornovyi@gmail.com"
-ZOHO_GRANT_TOKEN = "1000.58ee4e53d9f5e522e2d842d66d4e08a9.62282898ecfb46fdc051f20db36bc8fe"
+ZOHO_GRANT_TOKEN = "1000.9e7799487e3bf285e5cb24c8e6fc7700.3d4a54cf2a7d353c8e30365df10c7d2c"
 ZOHO_CLIENT_ID = "1000.6UBJGFQ9ZBEU8R4OF9X35FZ4KROLJO"
 ZOHO_CLIENT_SECRET = "538947a51fc6138b864550f447e75d8c3ba2edd0c3"
 ZOHO_ACCOUNT_DOMEN = "eu"
@@ -25,16 +28,16 @@ ZOHO_NOTIFICATIONS_ENDPOINT = "/zoho/deals/change"
 GOOGLE_ANALYTICS_API_URL = "https://www.google-analytics.com"
 GOOGLE_ANALYTICS_COLLECT_ENDPOINT = "/collect"
 
-config = {
+CONFIG = {
     "apiBaseUrl": ZOHO_API_URL,
-    "token_persistence_path": "/Users/yehorzhornovyi/PycharmProjects/ga_zoho_integration/env/",
+    "token_persistence_path": "./",
     "currentUserEmail": ZOHO_LOGIN_EMAIL,
     "client_id": ZOHO_CLIENT_ID,
     "client_secret": ZOHO_CLIENT_SECRET,
     "redirect_uri": "https://coxit.co",
-    "accounts_url": "https://accounts.zoho."+ZOHO_ACCOUNT_DOMEN,
+    "accounts_url": "https://accounts.zoho." + ZOHO_ACCOUNT_DOMEN,
 }
-request_input_json = {
+REQUEST_INPUT_JSON = {
     "watch": [
         {
             "channel_id": "1000000068002",
@@ -46,69 +49,88 @@ request_input_json = {
         }
     ]
 }
-app = Flask(__name__)
+APP = Flask(__name__)
+
 
 def get_access_token():
-    if path.isfile(config['token_persistence_path'] + 'zcrm_oauthtokens.pkl'):
-        access_token = oauth_client.get_access_token(ZOHO_LOGIN_EMAIL)
+    if path.isfile(CONFIG['token_persistence_path'] + 'zcrm_oauthtokens.pkl'):
+        _access_token = oauth_client.get_access_token(ZOHO_LOGIN_EMAIL)
     else:
         oauth_tokens = oauth_client.generate_access_token(ZOHO_GRANT_TOKEN)
-        access_token = oauth_tokens.get_access_token()
-    return access_token
+        _access_token = oauth_tokens.get_access_token()
+    return _access_token
 
-@app.route(ZOHO_NOTIFICATIONS_ENDPOINT, methods=['POST'])
+
+@APP.route(ZOHO_NOTIFICATIONS_ENDPOINT, methods=['POST'])
 def respond():
     # get deals records
     auth_header = {"Authorization": "Zoho-oauthtoken " + access_token}
     module = request.json["module"]
     for id_str in request.json["ids"]:
-        resp = requests.get(url=ZOHO_API_URL + "/crm/v2/" + module + "/" + id_str,
-                            headers=auth_header)
-        if resp.status_code == 200:
-            current_stage = resp.json()["data"][0]["Stage"]
-            logger.info("id=" + id_str + ": current stage is " + current_stage)
-
-            # TODO:Get record from DB and compare stages,
-            #  if stage changed then make POST request to analytic
-            user = Visitor()
-
-            params = {
-                "v": "1",
-                "t": "event",
-                "tid": "UA-180087680-1",
-                "uid": user.generate_unique_id(),
-                "ec": "zoho_stage_change",
-                "ea": "stage_change",
-                "el": current_stage,
-                "ua": "Opera / 9.80"
-            }
-
-            resp = requests.post(url=GOOGLE_ANALYTICS_API_URL+GOOGLE_ANALYTICS_COLLECT_ENDPOINT,
-                                 params=params)
-            if resp.status_code == 200:
-                logger.info("Update succesfully send to Google Analytic")
+        try:
+            resp = requests.get(url=ZOHO_API_URL + "/crm/v2/" + module + "/" + id_str,
+                                headers=auth_header)
+            resp.raise_for_status()
+        except requests.RequestException as ex:
+            LOGGER.error("The application can not get access to Zoho. Check the access token")
+            LOGGER.exception(ex)
         else:
-            logger.error("The application can not get access to Zoho. Check the access token")
+            try:
+                current_stage = resp.json()["data"][0]["Stage"]
+                LOGGER.info("id=" + id_str + ": current stage is " + current_stage)
+            except IndexError as ex:
+                LOGGER.exception(ex)
+                return Response(status=500)
+            else:
+                # TODO:Get record from DB and compare stages,
+                #  if stage changed then make POST request to analytic
+                user = Visitor()
+
+                params = {
+                    "v": "1",
+                    "t": "event",
+                    # TODO Change initialization of tid and uid parameters.
+                    "tid": "UA-180087680-1",
+                    "uid": user.generate_unique_id(),
+                    "ec": "zoho_stage_change",
+                    "ea": "stage_change",
+                    "el": current_stage,
+                    "ua": "Opera / 9.80"
+                }
+                try:
+                    resp = requests.post(url=GOOGLE_ANALYTICS_API_URL +
+                                             GOOGLE_ANALYTICS_COLLECT_ENDPOINT,
+                                         params=params)
+                    resp.raise_for_status()
+                except requests.RequestException as ex:
+                    LOGGER.error(ex)
+                    return Response(status=401)
+                else:
+                    LOGGER.info("Update succesfully send to Google Analytic")
 
     return Response(status=200)
 
+
 if __name__ == '__main__':
 
-    zoho_crm.ZCRMRestClient.initialize(config)
+    zoho_crm.ZCRMRestClient.initialize(CONFIG)
     oauth_client = zoho_crm.ZohoOAuth.get_client_instance()
     # Enable Zoho Notifications
     access_token = None
     try:
         access_token = get_access_token()
-    except Exception as e:
-        logger.error(e)
+    except zoho_crm.OAuthUtility.ZohoOAuthException as e:
+        LOGGER.exception(e)
+        sys.exit("Access token data is invalid")
 
-    if access_token:
-        header = {"Authorization": "Zoho-oauthtoken " + access_token,
-                  'Content-type': 'application/json'}
+    header = {"Authorization": "Zoho-oauthtoken " + access_token,
+              'Content-type': 'application/json'}
+    try:
         resp = requests.post(url=ZOHO_API_URL + ENABLE_NOTIFICATIONS_ENDPOINT, headers=header,
-                             data=json.dumps(request_input_json))
-        if not resp:
-            logger.error("Can`t access to ZohoCRM. Check if grant token and domen is valid")
-        else:
-            app.run(host="127.0.0.1", port=5050)
+                             data=json.dumps(REQUEST_INPUT_JSON))
+        resp.raise_for_status()
+    except requests.RequestException as ex:
+        LOGGER.error("Can`t access to ZohoCRM. Check if grant token and domen is valid")
+        LOGGER.exception(ex)
+    else:
+        APP.run(host="127.0.0.1", port=5050)
